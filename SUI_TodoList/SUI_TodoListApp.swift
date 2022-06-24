3389c10fbc08f808724ae37a29f498af5b0ec569
GimmyFox <mguzeev99@gmail.com>
2022-06-24 17:30:39 +0300

    Initial Commit

---
+++ SUI_TodoList/SUI_TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUI_TodoListApp.swift
+//  SUI_TodoList
+//
+//  Created by Maksim Guzeev on 24.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SUI_TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
